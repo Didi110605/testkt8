1ae07c55d7322abf6a31a234e853f97a5fe3ee47
Didi110605 <150129924+Didi110605@users.noreply.github.com>
2025-06-10 22:58:39 +0500

    Update test.py

--- test.py
+++ test.py
@@ -21,30 +21,31 @@ class YouTubeGesturesTest(unittest.TestCase):
         self.driver.quit()
 
     def test_scroll(self):
-        self.driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Trending")')
         time.sleep(2)
+        self.driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Trending")')
 
     def test_long_press(self):
+        time.sleep(2)  
         video_item = self.driver.find_element_by_xpath('(//android.view.ViewGroup[@content-desc="Video item"])[1]')
         self.action.long_press(video_item, duration=3000).release().perform()
-        time.sleep(2)
 
     def test_swipe(self):
+        time.sleep(2) 
         window_size = self.driver.get_window_size()
         start_x = window_size['width'] / 2
         start_y = window_size['height'] * 0.8
         end_y = window_size['height'] * 0.2
         self.action.press(x=start_x, y=start_y).wait(1000).move_to(x=start_x, y=end_y).release().perform()
-        time.sleep(2)
 
     def test_tap(self):
+        time.sleep(2)  
         search_button = self.driver.find_element_by_id('com.google.android.youtube:id/menu_item_1')
         self.action.tap(search_button).perform()
-        time.sleep(2)
+        time.sleep(2) 
         self.assertTrue(self.driver.find_element_by_id('com.google.android.youtube:id/search_edit_text'))
 
-
     def test_pinch_zoom(self):
+        time.sleep(2)  
         el = self.driver.find_element_by_id('com.google.android.youtube:id/player_view')
         multi_action = MultiAction(self.driver)
         action1 = TouchAction(self.driver)
@@ -60,7 +61,6 @@ class YouTubeGesturesTest(unittest.TestCase):
 
         multi_action.add(action1, action2)
         multi_action.perform()
-        time.sleep(2)
 
 if __name__ == '__main__':
     unittest.main()
